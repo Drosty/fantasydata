5dbd9c4ad44272f72d4dd67d2938e5f4c11e710e
Ryan Drost <ryan.s.drost@gmail.com>
2015-07-30 20:40:21 -0500

    week last completed

--- lib/fantasydata/api/week.rb
+++ lib/fantasydata/api/week.rb
@@ -9,6 +9,10 @@ module Fantasydata
         integer_from_response(:get, '/nfl/v2/JSON/CurrentWeek')
       end
 
+      def week_last_completed
+        integer_from_response(:get, '/nfl/v2/JSON/LastCompletedWeek')
+      end
+
     end
   end
 end
--- spec/fantasydata/api/week_spec.rb
+++ spec/fantasydata/api/week_spec.rb
@@ -26,4 +26,27 @@ describe Fantasydata::API::DailyFantasy do
     end
   end
 
+  describe '#week_last_completed' do
+    before do
+      stub_get("/nfl/v2/JSON/LastCompletedWeek").
+      to_return(:body => fixture("week/last_completed.json"),
+                 :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+
+    it "requests correct resource - pass string" do
+      @client.week_last_completed
+      expect(a_get("/nfl/v2/JSON/LastCompletedWeek")).to have_been_made
+    end
+
+    it "returns player details" do
+      current_week = @client.week_last_completed
+
+      expect(current_week).to be_an Integer
+      expect(current_week).to eq 4
+    end
+  end
+
+
+  
+
 end
