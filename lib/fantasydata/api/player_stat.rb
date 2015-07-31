6f91f743efe98a11f312cc9b4a8e29ed6ec51323
Ryan Drost <rdrost@nerdery.com>
2015-07-31 07:57:05 -0500

    player_stat_by_week_projection implemented

--- lib/fantasydata/api/player_stat.rb
+++ lib/fantasydata/api/player_stat.rb
@@ -9,6 +9,10 @@ module Fantasydata
         object_from_response(Fantasydata::PlayerStat, :get, "/nfl/v2/JSON/PlayerGameStatsByPlayerID/#{year}/#{week}/#{player_id}")
       end
 
+      def player_stat_by_week_projection year, week, player_id
+        object_from_response(Fantasydata::PlayerStat, :get, "http://api.nfldata.apiphany.com/nfl/v2/JSON/PlayerGameProjectionStatsByPlayerID/#{year}/#{week}/#{player_id}")
+      end
+      
     end
   end
 end
--- spec/fantasydata/api/player_stat_spec.rb
+++ spec/fantasydata/api/player_stat_spec.rb
@@ -28,4 +28,24 @@ describe Fantasydata::API::PlayerStat do
     end
   end
 
+  describe '#player_stat_by_week_projected' do
+    before do
+      stub_get("/nfl/v2/JSON/PlayerGameProjectionStatsByPlayerID/2014/12/7328").
+      to_return(:body => fixture("player_stat/stat_by_game_projection.json"),
+                 :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+
+    it "requests correct resource" do
+      @client.player_stat_by_week_projection(2014, 12, 7328)
+      expect(a_get("/nfl/v2/JSON/PlayerGameProjectionStatsByPlayerID/2014/12/7328")).to have_been_made
+    end
+
+    it "returns player details" do
+      stat = @client.player_stat_by_week_projection(2014, 12, 7328)
+
+      expect(stat).to be_an Fantasydata::PlayerStat
+      expect(stat.player_id).to eq 7328
+    end
+  end
+
 end
