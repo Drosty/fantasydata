91066647af856f56922bfe3237037252ae7d055b
Ryan Drost <ryan.s.drost@gmail.com>
2015-08-02 07:53:43 -0500

    injuries_by_year_week_team

--- lib/fantasydata/api/injury.rb
+++ lib/fantasydata/api/injury.rb
@@ -9,6 +9,10 @@ module Fantasydata
         objects_from_response(Fantasydata::Injury, :get, "/nfl/v2/JSON/Injuries/#{year}/#{week}")
       end
 
+      def injuries_by_year_week_team year, week, team
+        objects_from_response(Fantasydata::Injury, :get, "/nfl/v2/JSON/Injuries/#{year}/#{week}/#{team}")
+      end
+
     end
   end
 end
--- spec/fantasydata/api/injury_spec.rb
+++ spec/fantasydata/api/injury_spec.rb
@@ -6,7 +6,7 @@ describe Fantasydata::API::Injury do
     @client = new_test_client
   end
 
-  describe '#injury_by_year_and_week' do
+  describe '#injuries_by_year_and_week' do
     before do
       stub_get("/nfl/v2/JSON/Injuries/2014/5").
       to_return(:body => fixture("injury/by_year_and_week.json"),
@@ -28,6 +28,27 @@ describe Fantasydata::API::Injury do
     end
   end
 
+  describe '#injuries_by_year_week_team' do
+    before do
+      stub_get("/nfl/v2/JSON/Injuries/2014/5/MIN").
+      to_return(:body => fixture("injury/by_team.json"),
+                 :headers => {:content_type => "application/json; charset=utf-8"})
+    end
+
+    it "requests correct resource" do
+      @client.injuries_by_year_week_team(2014, 5, 'MIN')
+      expect(a_get("/nfl/v2/JSON/Injuries/2014/5/MIN")).to have_been_made
+    end
+
+    it "returns injuries" do
+      injuries = @client.injuries_by_year_week_team(2014, 5, 'MIN')
+
+      expect(injuries).to be_an Array
+      expect(injuries.first).to be_an Fantasydata::Injury
+      expect(injuries.first.injury_id).to eq 31739
+      expect(injuries.first.player_id).to eq 14461
+    end
+  end
 
 
 end
